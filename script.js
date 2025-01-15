10b2b4930a7975ce0f2251aa904c3f23d2f86ad7
GDPlayer <someguyyounotknow@gmail.com>
2025-01-15 13:02:39 +0800

    fix! yay!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function copy() {
     //copy text "curl nfetch.neocities.org | cmd"
-    navigator.clipboard.writeText("curl nfetch.pages.dev | cmd");
+    navigator.clipboard.writeText("curl -L nfetch.pages.dev | cmd");
 }
